b28221ccbc6b1aacd7ebc8575ccd4c967167b9d5
Anna Lia Lira <annalialira@gmail.com>
2024-04-23 09:49:58 -0300

    .

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // conexao com o banco 
 include_once 'conexao.php';
 ?>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
  <link rel="stylesheet" href="./style.css?v=<?php echo rand(0,999999);?>">
  <title>Pobreza Menstrual</title>
