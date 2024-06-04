8c2cb288241dda4ef04db932e519029e9b4ab904
Anna Lia Lira <annalialira@gmail.com>
2024-06-04 11:19:18 -0300

    Update pobreza.sql

--- db/pobreza.sql
+++ db/pobreza.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04/06/2024 às 16:15
+-- Tempo de geração: 04/06/2024 às 16:18
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
