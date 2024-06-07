1cf493e5b35b9c635be94854cd1d7600dddede2d
Anna Lia Lira <annalialira@gmail.com>
2024-06-07 09:25:13 -0300

    up

--- index.php
+++ index.php
@@ -3,12 +3,11 @@
 include_once 'conexao.php';
 ?>
 
- <link rel="stylesheet" href="./style.css?v=<?php echo rand(0,999999);?>">
- <title>Pobreza Menstrual</title>
+ <link rel="stylesheet" href="style.css">;
+ <title>Pobreza Menstrual no Sistema Carcerário Brasileiro</title>
  <body >
 
-<header class="row">     
-<nav class="navbar col-12 navbar-dark bg-dark" id="hh">
+<header class="row">
   <a class="navbar-brand ml-2"  href="#">Pobreza Menstrual</a>
   
 </nav>
@@ -16,14 +15,16 @@ include_once 'conexao.php';
 <main id="index" class="container">
           <div class="row">
           <?php 
-          $sql = "SELECT * FROM pobreza";
+          $sql = "SELECT * FROM artigo";
           // executa o comando SQL no banco e retornar os dados
           $result = mysqli_query( $conexao, $sql );
           // laco de repeticao 
-          while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){       
+          while($row = mysqli_fetch_array($result)){       
           ?>
           <div class="col-6 ">
-            <a href="questao.php?area=<?php echo $row['id'];?>"><div><img class="imagens" src="./img/<?php echo $row['foto'];?>" alt=""></div></a>
+            <a href="artigo.php?id=<?php echo $row['artigoid'];?>"><?php echo $row['titulo'];?></a>
+            
+          
             
           </div>
           <?php
@@ -32,6 +33,3 @@ include_once 'conexao.php';
  </div> 
  </header>
  </body>
- <?php 
-include_once 'footer.php';
-?>
