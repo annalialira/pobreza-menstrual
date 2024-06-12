348a11753d8521fad833536182a5637788581081
Anna Lia Lira <annalialira@gmail.com>
2024-06-12 11:49:57 -0300

    proejtofinal

---
+++ artigo.php
@@ -0,0 +1,28 @@
+<?php 
+include_once 'conexao.php';
+
+
+
+$artigoid = $_GET['id'];
+?>
+ <link rel="stylesheet" href="style.css?t=<?php echo date('YmdHis').rand(1,999999)?>">
+
+<?php 
+    $sql = "SELECT * FROM artigo WHERE artigoid = $artigoid";
+    $result = mysqli_query( $conexao, $sql );
+    $row = mysqli_fetch_array($result);    
+    ?>
+    <div class="outrapagina">
+    <h2><a class="linkcor" href="artigo.php?id=<?php echo $row['artigoid'];?>"><?php echo $row['titulo'];?></a></h2>
+    <p class="textoartigo "href="artigo.php?id=<?php echo $row['artigoid'];?>"><?php echo $row['subtitulo'];?></p>
+    <p href="artigo.php?id=<?php echo $row['artigoid'];?>"><?php echo $row['texto'];?></p>
+    <p href="artigo.php?id=<?php echo $row['artigoid'];?>"><?php echo $row['autor'];?></p>
+    </div>
+    <footer id="sticky-footer">
+    <div class="container text-center">
+      <small>Giovanna Pandolfo &copy; Anna Lia</small>
+    </div>
+  </footer>
+<?php
+
+          
--- index.php
+++ index.php
@@ -3,16 +3,18 @@
 include_once 'conexao.php';
 ?>
   
- <link rel="stylesheet" href="style.css">
+ <link rel="stylesheet" href="style.css?t=<?php echo date('YmdHis').rand(1,999999)?>">
  <link rel="preconnect" href="https://fonts.googleapis.com">
  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
  <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text:ital@0;1&display=swap" rel="stylesheet">
 
  <title>Pobreza Menstrual no Sistema Carcerário Brasileiro</title>
+</head>
+
  <body>
 
-<header>
-  <h1 class="primeiro">Pobreza Menstrual no Sistema Carcerário Brasileiro</h1>
+<header >
+  <h1 class="titulo">Pobreza Menstrual no Sistema Carcerário Brasileiro</h1>
 </header> 
 
 </php>
@@ -26,14 +28,19 @@ include_once 'conexao.php';
           while($row = mysqli_fetch_array($result)){       
           ?>
           <div class="block">
-            <h2><a href="artigo.php?id=<?php echo $row['artigoid'];?>"><?php echo $row['titulo'];?></a></h2>
+            <h2><a class = "cordolink" href="artigo.php?id=<?php echo $row['artigoid'];?>"><?php echo $row['titulo'];?></a></h2>
             <p href="artigo.php?id=<?php echo $row['artigoid'];?>"><?php echo $row['subtitulo'];?></p>
+            
           </div>
+          
           <?php
           }
           ?>
+  </div>
  </div>
- <footer>
-  
+ <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small >Giovanna Pandolfo &copy; Anna Lia</small>
+    </div>
   </footer>
  </body>
