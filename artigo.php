613e695c89d57347b00cc423a8054bc62a9e079a
Giovanna Pandolfo <giovannapandolfo1106@gmail.com>
2024-06-28 08:27:22 -0300

    update

--- artigo.php
+++ artigo.php
@@ -9,6 +9,7 @@ $artigoid = $_GET['id'];
  <link rel="preconnect" href="https://fonts.googleapis.com">
  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
  <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text:ital@0;1&display=swap" rel="stylesheet">
+ <link rel="shortcut icon" type="imagex/png" href="img/icone.png">
 
  <title>Pobreza Menstrual no Sistema Carcerário Brasileiro</title>
 </head>
